27e931e5115f485e870becb375ff023c6cfe9b1f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-06 11:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 //your JS code here. If required.
-const blockIdInput = document.getElementById("block_id");
-  const colorInput = document.getElementById("colour_id");
-  const changeButton = document.getElementById("change_button");
-  const resetButton = document.getElementById("reset_button");
-  const gridItems = document.querySelectorAll(".grid-item");
+const gridItems = document.querySelectorAll(".grid-item");
+const changeColor = document.getElementById("change_button");
+const resetButton = document.getElementById("reset_button");
 
-  changeButton.addEventListener("click", function () {
-      const blockId = blockIdInput.value;
-      const color = colorInput.value;
-
-      gridItems.forEach(item => {
-          item.style.backgroundColor = "transparent";
-      });
-      const selectedBlock = document.getElementById(blockId);
-	  if (selectedBlock) {
-          selectedBlock.style.backgroundColor =  color;
-      } else {
-          alert("Block ID not found!");
-      }
-  });
-
-  resetButton.addEventListener("click", function () {
-      gridItems.forEach(item => {
-          item.style.backgroundColor = "transparent";
-      });
+changeColor.addEventListener('click', () => {
+	const color = document.getElementById("colour_id").value;
+	const blockId = document.getElementById("block_id").value;
+	const selectBlock = document.getElementById(blockId);
+	if(selectBlock){
+		gridItems.forEach((item)=>{
+			item.style.backgroundColor = "transparent";
+		}); 
+		selectBlock.style.backgroundColor = color;
+	}else{
+		alert("Invalid block Id. Please provide correct Id... ");
+	}
+ });
+	resetButton.addEventListener('click', ()=>{
+		gridItems.forEach((item)=>{
+			item.style.backgroundColor = "transparent";
+		});
   });
