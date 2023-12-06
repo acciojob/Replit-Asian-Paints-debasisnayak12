5c284872c4ea0846f6eac1cc4b4ee29d46aadeba
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-06 10:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const gridItems = document.querySelectorAll(".grid-item");
+const changeColor = document.getElementById("change_button");
+const resetButton = document.getElementById("Reset");
+
+changeColor.addEventListener('click', () => {
+	const color = document.getElementById("color_id").value;
+	const blockId = document.getElementById("block_id").value;
+
+	const selectBlock = document.getElementById(blockId);
+	if(selectBlock){
+		gridItems.forEach((item)=>{
+			item.style.backgroundColor = "transparent";
+		});
+		selectBlock.style.backgroundColor = color;
+	}else{
+		alert("Invalid block Id. Please provide correct Id... ");
+	}
+ });
+
+	resetButton.addEventListener('click', ()=>{
+		gridItems.forEach((item)=>{
+			item.style.backgroundColor = "transparent";
+		});
+  });
